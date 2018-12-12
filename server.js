c2a8c469e4a98b3073f76844fd3a2a08fd478a06
Elad Dahan <eladdahan20@gmail.com>
2018-12-12 18:08:07 +0200

    server change

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+
+const port = process.env.PORT || '3000';
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running');
 });
